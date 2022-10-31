79c21b60addf1473e11c835971a14372af757b1d
David Knight <davidmlknight@gmail.com>
2022-10-30 22:52:01 -0700

    added shell with help from everyone in paired programming method

--- stockInfinity/stockInf/urls.py
+++ stockInfinity/stockInf/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('search/', views.search, name='search'),
+        path('account/', views.account, name='account'),
         ]
 
