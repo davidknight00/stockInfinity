a1f80e41be028cdb8672f74f27ca2e13dbbe4ff1
David Knight <davidmlknight@gmail.com>
2022-11-06 11:35:59 -0700

    added tar file, modified some settings for prod servers (commented out until in prod)

--- stockInfinity/stockInfinity/settings.py
+++ stockInfinity/stockInfinity/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*aqf(e@z2__5av2f0(t$#isermj$9tqkw2dfq*xzphe91&f)uu'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
